25ac7d7209b03533ab1defa9cb5048e7404f4525
lums <jeremyondrey@gmail.com>
2021-07-17 20:49:32 +0200

    WIP dynamic routing

---
+++ src/routes/level/[letter].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+import { page } from "$app/stores";
+
+
+    export const load = ({page})=>{
+        
+        const letter = page.params.letter;
+        
+        return {
+            props: {
+                letter,
+            },
+        };
+    };
+</script>
+
+<script>
+    export let letter;
+</script>
+
+<div class="note-page">
+    letter {letter}
+    
+</div>
