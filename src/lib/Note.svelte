c5863211e1696263979e93f4deb500e33ac41904
lumsdnb <jeremy@lums.io>
2021-07-19 12:25:52 +0200

    Add note component

---
+++ src/lib/Note.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let sharp = false;
+  export let flat = false;
+  export let active = false;
+</script>
+
+<div class="note">
+  {#if sharp}
+    #
+  {/if}
+  {#if flat}
+    b
+  {/if}
+  {#if active}
+    <span class="active-note"> o </span>
+  {:else}
+    <span class="inactive-note">o</span>
+  {/if}
+</div>
+
+<style>
+  .entered-notes > span {
+    display: inline-block;
+    font-size: 1.4rem;
+    filter: drop-shadow(6px 6px 5px black);
+  }
+  .note {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    color: white;
+  }
+  .active-note {
+    color: gold;
+  }
+  .inactive-note {
+    color: white;
+  }
+</style>
--- src/lib/NoteLines.svelte
+++ src/lib/NoteLines.svelte
@@ -1,92 +1,83 @@
 <script>
-	import { spring } from 'svelte/motion';
+  import { spring } from 'svelte/motion';
+  import Note from '$lib/Note.svelte';
 </script>
 
 <picture>
-	<source srcset="/assets/clef-white.png" type="image/png" />
-	<img src="/assets/clef-white.png" alt="Welcome" />
+  <source srcset="/assets/clef-white.png" type="image/png" />
+  <img src="/assets/clef-white.png" alt="Welcome" />
 </picture>
-<div class="notes">
-	<div class="lines">
-		<div class="note-line" />
-		<div class="note-line" />
-		<div class="note-line" />
-		<div class="note-line" />
-		<div class="note-line" />
-	</div>
-	<div class="entered-notes">
-		<span class="inactive-note">o</span>
-		<span class="active-note">o</span>
-		<span class="inactive-note">o</span>
-	</div>
+<div class="container">
+  <div class="lines">
+    <div class="note-line" />
+    <div class="note-line" />
+    <div class="note-line" />
+    <div class="note-line" />
+    <div class="note-line" />
+  </div>
+  <div class="entered-notes">
+    <Note active />
+    <Note sharp />
+    <Note flat />
+  </div>
 </div>
 
 <style>
-	.notes {
-		display: flex;
-		flex-direction: row;
-	}
-	.entered-notes{
-		display: flex;
-		flex-direction: column;
-		position: relative;
-		right: 4rem;
-	}
-	.entered-notes >span {
-		display: inline-block;
-		font-size: 1.4rem;
-		filter: drop-shadow(6px 6px 5px black);
-	}
-	.active-note{
-		color: gold;
-	}
-	.inactive-note{
-		color: white;
-	}
-	img[src*='clef-white.png'] {
-		width: 7rem;
-		position: relative;
-		top: 7.5rem;
-		right: 14rem;
-		filter: drop-shadow(6px 6px 5px black);
-	}
-	.note-line {
-		width: 70vw;
-		height: 2px;
-		background-color: white;
-		margin-bottom: 1.1rem;
-	}
-	.counter {
-		display: flex;
-		border-top: 1px solid rgba(0, 0, 0, 0.1);
-		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-		margin: 1rem 0;
-	}
+  .entered-notes {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    right: 4rem;
+  }
 
-	.counter button {
-		width: 2em;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 0;
-		background-color: transparent;
-		color: var(--text-color);
-		font-size: 2rem;
-	}
+  .container {
+    display: flex;
+    flex-direction: row;
+  }
+  img[src*='clef-white.png'] {
+    width: 7rem;
+    position: relative;
+    top: 7.5rem;
+    right: 14rem;
+    filter: drop-shadow(6px 6px 5px black);
+  }
+  .note-line {
+    width: 70vw;
+    height: 2px;
+    background-color: white;
+    margin-bottom: 1.1rem;
+  }
+  .counter {
+    display: flex;
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    margin: 1rem 0;
+  }
 
-	.counter button:hover {
-		background-color: var(--secondary-color);
-	}
+  .counter button {
+    width: 2em;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 0;
+    background-color: transparent;
+    color: var(--text-color);
+    font-size: 2rem;
+  }
 
-	svg {
-		width: 25%;
-		height: 25%;
-	}
+  .counter button:hover {
+    background-color: var(--secondary-color);
+  }
 
-	path {
-		vector-effect: non-scaling-stroke;
-		stroke-width: 2px;
-		stroke: var(--text-color);
-	}
+  svg {
+    width: 25%;
+    height: 25%;
+  }
+
+  path {
+    vector-effect: non-scaling-stroke;
+    stroke-width: 2px;
+    stroke: var(--text-color);
+  }
 </style>
