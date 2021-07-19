d5d1dcf4cbce42b12f71c0a62703af77091d3245
lumsdnb <jeremy@lums.io>
2021-07-19 23:43:52 +0200

    Cleanup unused css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,12 @@
 
 <script>
   import Counter from '$lib/Counter.svelte';
-  import NoteLines from '$lib/NoteLines.svelte';
+  import NoteRenderer from '$lib/NoteRenderer.svelte';
   import NoteInput from '$lib/NoteInput.svelte';
   import MessageBox from '$lib/MessageBox.svelte';
+
+  //this will be loaded from main json file later
+  const scale = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
 </script>
 
 <svelte:head>
@@ -15,9 +18,9 @@
 
 <section>
   <MessageBox />
-  <NoteLines />
+  <NoteRenderer notes={scale} />
   <NoteInput />
-  <MessageBox message={"yooo"} player/>
+  <MessageBox message={'yooo'} player />
 </section>
 
 <style>
@@ -27,24 +30,6 @@
     justify-content: center;
     align-items: center;
     flex: 1;
-  }
-
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
+    color: white;
   }
 </style>
