a03610e1ec59c2c27e438668a8d2f6a55395bdd6
lumsdnb <jeremy@lums.io>
2021-07-20 13:54:48 +0200

    Layout hacks, someone could probably do this nicer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,9 +17,14 @@
 
 <section>
   <MessageBox />
-  <NoteRenderer notes={scale} />
-  <NoteInput />
-  <MessageBox message={'yooo'} player />
+  <NoteRenderer {scale} />
+  <div class="shiftdown">
+    <NoteInput />
+  </div>
+  <!-- those divs pretty hacky and not very precise -->
+  <div class="bottom">
+    <MessageBox message={'yooo'} player />
+  </div>
 </section>
 
 <style>
@@ -30,5 +35,15 @@
     align-items: center;
     flex: 1;
     color: white;
+  }
+  .shiftdown {
+    margin-top: 5rem;
+  }
+  .bottom {
+    position: absolute;
+    bottom: 1rem;
+    width: 100%;
+    left: 50%;
+    transform: translateX(-45%);
   }
 </style>
