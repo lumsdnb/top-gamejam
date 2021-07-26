e637e75cbb9f03f2a7a05ba5e01ed6351ea0131a
SizweMalobola <25reyals@gmail.com>
2021-07-26 19:48:34 +0200

    about page is now responsive @320px

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,40 +1,50 @@
 <script context="module">
-	import { browser, dev } from '$app/env';
+  import { browser, dev } from "$app/env";
 
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
+  // we don't need any JS on this page, though we'll load
+  // it in dev so that we get hot module replacement...
+  export const hydrate = dev;
 
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
+  // ...but if the client-side router is already loaded
+  // (i.e. we came here from elsewhere in the app), use it
+  export const router = browser;
 
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
+  // since there's no dynamic data here, we can prerender
+  // it so that it gets served as a static asset in prod
+  export const prerender = true;
 </script>
 
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
-<div class="content">
-	<h1>About this game</h1>
+<h1 id="title">About</h1>
+<div id="content">
+  <p>[gamename] teaches you music theory in a fun way.</p>
 
-	<p>
-		[gamename] teaches you music theory in a fun way. 
-	</p>
-
-	<p>
-		This project was created as part of the first <a href="https://itch.io/jam/top-jam-1">Odin game jam</a>.
-	</p>
-	<p>don't expect this to work yet..</p>
+  <p>
+    This project was created as part of the first <a
+      href="https://itch.io/jam/top-jam-1">Odin game jam</a
+    >.
+  </p>
+  <p>don't expect this to work yet..</p>
 </div>
 
 <style>
-	.content {
-		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
-	}
+  @media (min-width: 320px) {
+    #title {
+      font-size: 1.5rem;
+      text-transform: capitalize;
+      margin-bottom: 1em;
+      text-shadow: 1px 1px 2px #ff3e00;
+    }
+    #content {
+      background-color: rgba(129, 129, 129, 0.418);
+      font-size: 1rem;
+      padding: 0.4em;
+    }
+    #content p {
+      margin-bottom: 1em;
+    }
+  }
 </style>
