60601ca3043a0b7d250aafb9323419df9332eb63
lumsdnb <jeremy@lums.io>
2021-07-30 12:06:01 +0200

    Update scroll w text

--- src/lib/Scroll.svelte
+++ src/lib/Scroll.svelte
@@ -2,11 +2,28 @@
   export let type = 'c';
 </script>
 
-<div class="scroll">
-  <img src={`/static/scrolls/${type}-major-scroll.png`} alt="" />
+<div class="scroll-container">
+  <div class="scroll">
+    <img src={`/static/scrolls/${type}-major-scroll.png`} alt="" />
+  </div>
+  <div class="scroll-text">
+    <p>chords are build from different notes of the major scale</p>
+    <p>1st 3rd and 5th create a major chord</p>
+  </div>
 </div>
 
 <style>
+  p {
+    color: black;
+  }
+  .scroll-text {
+    position: absolute;
+    font-size: 0.75rem;
+    bottom: 15rem;
+    left: 5rem;
+    max-width: 14rem;
+    z-index: 4;
+  }
   @media (min-width: 900px) {
     .scroll {
       display: flex;
@@ -14,6 +31,7 @@
       position: relative;
       bottom: 3rem;
     }
+
     img {
       max-width: 366px;
       max-height: 50%;
