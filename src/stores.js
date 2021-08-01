dbde9549b2ab83c5f55196a6e1fd186f9f98daf8
lumsdnb <jeremy@lums.io>
2021-08-01 15:42:29 +0200

    Simplify character messages

--- src/stores.js
+++ src/stores.js
@@ -20,7 +20,9 @@ export const gameData = writable({
   //3 - first game played
   //4 if answer is wrong
   //5 if answer is correct
+
   tutorialState: 0,
+  finishedTutorial: false,
   showScroll: false,
   canProgress: false,
   canPresent: true,
@@ -46,52 +48,40 @@ export const messageSystem = readable([
   ['well that was.. something. i wonder what else i will find here..', ''],
 ]);
 
-const characterC = {
-  name: 'c',
-  messages: [
+export const characterMessages = readable({
+  c: [
     `It's been so long since I heared my favorite chord.`,
     `YES, this is the one, thank you.`,
-    `I don't like this chord.`],
-};
-const characterD = {
-  name: 'd',
-  messages: [
+    `I don't like this chord.`,
+  ],
+  d: [
     `I heard you play chords for the folks in this forest, show me something for my taste.`,
     `You got some talent boy, good job.`,
-    `This doesn't cut for me.`],
-};
-const characterE = {
-  name: 'e',
-  messages: [
+    `This doesn't cut for me.`,
+  ],
+  e: [
     `It's so annoying to live with this shape, my leg alway gets tangled in vines.`,
     `I like this tune, keep it up!.`,
-    `This is not what I like.`],
-};
-const characterF = {
-  name: 'f',
-  messages: [
+    `This is not what I like.`,
+  ],
+  f: [
     `My little brother E keeps complaining about getting tangled in vines, but there are no vines in this forest.`,
     `This chord is music to my ears, even tho I don't have ears.`,
-    `Try a different one.`],
-};
-const characterG = {
-  name: 'g',
-  messages: [
+    `Try a different one.`,
+  ],
+  g: [
     `The forest is filled with snakes, but they are more scared of you that you are scared to them.`,
     `Very well played! You are going places.`,
-    `I didn't enjoy this a bit.`],
-};
-const characterA = {
-  name: 'a',
-  messages: [
+    `I didn't enjoy this a bit.`,
+  ],
+  a: [
     `At night the fireflies will lit the forrest up. It's the most beautiful thing you'll ever see.`,
     `This was amazing.`,
-    `I bet you can do something better.`],
-};
-const characterB = {
-  name: 'b',
-  messages: [
+    `I bet you can do something better.`,
+  ],
+  b: [
     `I heared you playing amazing music on your harp, show me something.`,
     `It's been a while since I heared something this beautiful.`,
-    `Is that all you've got?`],
-};
+    `Is that all you've got?`,
+  ],
+});
