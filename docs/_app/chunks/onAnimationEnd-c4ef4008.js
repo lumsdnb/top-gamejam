9cc2523e02d8afa4ae576b0c80cca289c31ced5f
lums <jeremyondrey@gmail.com>
2021-08-03 15:01:51 +0200

    Rm build experiments, will retry later

--- docs/_app/chunks/onAnimationEnd-c4ef4008.js
+++
@@ -1,1 +0,0 @@
-const t=(t,e)=>{new MutationObserver((t=>{t.forEach((t=>{const s="attributes"===t.type,r=!t.target.classList.contains("typing");s&&r&&e()}))})).observe(t,{attributes:!0,childList:!0,subtree:!0})};export{t};
