5427f0941fda3f3989cc0c096eadf56375ec1d90
lums <jeremyondrey@gmail.com>
2021-08-04 20:42:18 +0200

    works locally..

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    ssr: false,
     adapter: adapter(),
   },
 };
