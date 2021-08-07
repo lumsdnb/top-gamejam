b0d23fea53f1b626816f5e4780f7b83ea07fe0ac
lumsdnb <jeremy@lums.io>
2021-08-07 11:24:48 +0200

    Add back big next button

--- src/lib/MessageBox.svelte
+++ src/lib/MessageBox.svelte
@@ -9,14 +9,13 @@
   export let wizard = false;
   const showNextButton = () => {
     if (
-         $gameData.tutorialState!==0
-      && $gameData.tutorialState!==4
-      && $gameData.tutorialState!==5
-      && $gameData.tutorialState!==6
-      && $gameData.tutorialState!==7
-      && $gameData.tutorialState!==8 
-      ) {
-      
+      $gameData.tutorialState !== 0 &&
+      $gameData.tutorialState !== 4 &&
+      $gameData.tutorialState !== 5 &&
+      $gameData.tutorialState !== 6 &&
+      $gameData.tutorialState !== 7 &&
+      $gameData.tutorialState !== 8
+    ) {
       console.log('showing btn');
       document.getElementById('next-button').classList.remove('hidden');
     }
@@ -33,10 +32,10 @@
 <div class="message-box">
   {#if player}
     {#if $gameData.finishedTutorial}
-    <span class="gold-count" >{`${$gameData.gold} gold`}</span>
+      <span class="gold-count">{`${$gameData.gold} gold`}</span>
     {/if}
-  
-  <img id="player-img" src="/static/assets/favicon.png" alt="" />
+
+    <img id="player-img" src="/static/assets/favicon.png" alt="" />
     <!-- show btn when chord is built - todo: hide when  -->
     {#if $gameData.enteredNotes.length >= 3 && $gameData.canPresent}
       <button class="check-btn" on:click>present chord</button>
@@ -47,7 +46,9 @@
       </Typewriter>
     {/if}
 
-    <button id="next-button" class="hidden" on:click={nextStep}>next</button>
+    <button id="next-button" class="hidden" on:click={nextStep}>
+      <span>next</span></button
+    >
   {:else}
     <!-- opponent box -->
     <Typewriter interval={100} cursor={false} on:done={showNextButton}>
@@ -71,12 +72,11 @@
     visibility: hidden;
   }
 
-
-  .gold-count{
+  .gold-count {
     color: var(--accent-color2);
     position: absolute;
-    top: .5rem;
-    left: .5rem;
+    top: 0.5rem;
+    left: 0.5rem;
     filter: drop-shadow(2px 2px 2px black);
   }
   /* add media query */
@@ -93,7 +93,6 @@
     position: relative;
     user-select: none;
   }
-  
 
   img {
     max-width: 25%;
@@ -125,6 +124,30 @@
     height: 0.4em;
     position: absolute;
   }
+  #next-button {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    text-align: right;
+    background-color: transparent;
+    color: var(--accent-color2);
+    border: 1px solid var(--accent-color2);
+  }
+  #next-button > span {
+    position: relative;
+    top: 3.5rem;
+  }
+  #next-button:hover {
+    color: var(--accent-color);
+    border: 1px solid var(--accent-color);
+  }
+  #next-button:active {
+    color: var(--accent-color2);
+    border: 1px solid var(--accent-color2);
+  }
+
   .b1 {
     top: -0.2em;
     left: -0.2em;
@@ -142,77 +165,70 @@
     right: -0.2em;
   }
   @media (min-width: 320px) {
-  .message-box {
-    position: relative;
-  }
+    .message-box {
+      position: relative;
+    }
     .msg-txt {
-    padding: 0 1rem;
-    font-size: var(--fz5);
-    user-select: none;
-    /* text doesnt stretch the message box from its original height */
-    margin: 0;
-    overflow: auto;
-    max-height: 70.5px;
-  }
-   #next-button {
-    position: absolute;
-    bottom: 2px;
-    right: 2px;
-    background-color: transparent;
-    color: var(--accent-color2);
-    border: 1px solid var(--accent-color2);
-  }
-  #next-button:hover{
-    color: var(--accent-color);
-    border: 1px solid var(--accent-color);
-  }
-  #next-button:active{
-    color: var(--accent-color2);
-    border: 1px solid var(--accent-color2);
-  }
+      padding: 0 1rem;
+      font-size: var(--fz5);
+      user-select: none;
+      /* text doesnt stretch the message box from its original height */
+      margin: 0;
+      overflow: auto;
+      max-height: 70.5px;
+    }
+    #next-button > span {
+      top: 2.5rem;
+    }
   }
   @media (min-width: 414px) {
     .msg-txt {
-    padding: 0 1rem;
-    font-size: 1.1rem;
-    max-height: 90.5px;
-  }
+      padding: 0 1rem;
+      font-size: 1.1rem;
+      max-height: 90.5px;
+    }
     .box {
-    background-color: white;
-    width: .7em;
-    height: .7em;
-    position: absolute;
-  }
-   .b1 {
-    top: -0.4em;
-    left: -0.4em;
-  }
-  .b2 {
-    top: -0.4em;
-    right: -0.4em;
-  }
-  .b3 {
-    bottom: -0.4em;
-    left: -0.4em;
-  }
-  .b4 {
-    bottom: -0.4em;
-    right: -0.4em;
-  }
+      background-color: white;
+      width: 0.7em;
+      height: 0.7em;
+      position: absolute;
+    }
+    .b1 {
+      top: -0.4em;
+      left: -0.4em;
+    }
+    .b2 {
+      top: -0.4em;
+      right: -0.4em;
+    }
+    .b3 {
+      bottom: -0.4em;
+      left: -0.4em;
+    }
+    .b4 {
+      bottom: -0.4em;
+      right: -0.4em;
+    }
+    #next-button > span {
+      top: 5rem;
+    }
   }
   @media (min-width: 768px) {
     .msg-txt {
-    font-size: 2rem;
-    max-height: inherit;
-  }
-   #next-button {
-     font-size:  2rem;
-   }
-   .box {
-    background-color: white;
-    width: 1em;
-    height: 1em;
-    position: absolute;
-  }
+      font-size: 2rem;
+      max-height: inherit;
+    }
+    #next-button {
+      font-size: 2rem;
+    }
+    .box {
+      background-color: white;
+      width: 1em;
+      height: 1em;
+      position: absolute;
+    }
+    #next-button > span {
+      top: 7rem;
+    }
   }
 </style>
